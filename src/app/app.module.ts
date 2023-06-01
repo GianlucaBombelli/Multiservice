3224595db4598f8c8f852eb793855baab686b251
Gianluca Bombelli <gianluca.bombelli.work@gmail.com>
2023-06-01 16:07:21 +0200

    ottavo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -55,6 +55,7 @@ import { RegisterformComponent } from './registerform/registerform.component';
     AppRoutingModule,
     FormsModule,
     RouterModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/registerform/registerform.component.ts
+++ src/app/registerform/registerform.component.ts
@@ -1,5 +1,6 @@
 import { animate, style, transition, trigger } from '@angular/animations';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registerform',
@@ -14,7 +15,57 @@ import { Component } from '@angular/core';
     ])
   ]
 })
-export class RegisterformComponent {
+export class RegisterformComponent implements OnInit{
+
+  registerForm!: FormGroup;
+  registerFormAzienda!: FormGroup;
+
+  constructor () {}
+
+  ngOnInit (): void {
+    this.registerForm = new FormGroup({
+      nome: new FormControl('',Validators.required),
+      email: new FormControl('',Validators.required),
+      password: new FormControl('',Validators.required),
+      codiceFiscale: new FormControl('',Validators.required),
+      nazione: new FormControl('',Validators.required),
+      cellulare: new FormControl('',Validators.required),
+      indirizzo: new FormControl('',Validators.required),
+      citta: new FormControl('',Validators.required),
+      cap: new FormControl('',Validators.required),
+      provincia: new FormControl('',Validators.required),
+      spedizioneCognomeNome: new FormControl('',Validators.required),
+      spedizioneIndirizzo: new FormControl('',Validators.required),
+      spedizioneCitta: new FormControl('',Validators.required),
+      spedizioneCap: new FormControl('',Validators.required),
+      spedizioneProvincia: new FormControl('',Validators.required),
+      spedizioneNazione: new FormControl('',Validators.required),
+      spedizioneCellulare: new FormControl('',Validators.required),
+    }),
+    this.registerFormAzienda = new FormGroup ({
+      ragioneSociale: new FormControl('',Validators.required),
+      email: new FormControl('',Validators.required),
+      nazione: new FormControl('',Validators.required),
+      password: new FormControl('',Validators.required),
+      codiceFiscale: new FormControl('',Validators.required),
+      partivaIva: new FormControl('',Validators.required),
+      codiceSdi: new FormControl('',Validators.required),
+      pec: new FormControl('',Validators.required),
+      indirizzo: new FormControl('',Validators.required),
+      citta: new FormControl('',Validators.required),
+      cap: new FormControl('',Validators.required),
+      provincia: new FormControl('',Validators.required),
+      cellulare: new FormControl('',Validators.required),
+      telefono: new FormControl('',Validators.required),
+      spedizioneRagioneSociale: new FormControl('',Validators.required),
+      spedizioneIndirizzo: new FormControl('',Validators.required),
+      spedizioneCitta: new FormControl('',Validators.required),
+      spedizioneCap: new FormControl('',Validators.required),
+      spedizioneProvincia: new FormControl('',Validators.required),
+      spedizioneNazione: new FormControl('',Validators.required),
+      spedizioneCellulare: new FormControl('',Validators.required),
+    })
+  }
 
   email: string = '';
   password: string = '';
@@ -59,78 +110,10 @@ export class RegisterformComponent {
   }
 
   onInviadati() {
-    console.log('Account:', this.isAzienda)
-    if (this.email !== '') {
-      console.log('email:', this.email);
-    }
-    if (this.password !== '') {
-      console.log('password:', this.password);
-    }
-    if (this.ragioneSociale !== '') {
-      console.log('ragioneSociale:', this.ragioneSociale);
-    }
-    if (this.cognomeNome !== '') {
-      console.log('cognomeNome:', this.cognomeNome);
-    }
-    if (this.confermapass !== '') {
-      console.log('confermapass:', this.confermapass);
-    }
-    if (this.nazione !== '') {
-      console.log('nazione:', this.nazione);
-    }
-    if (this.codiceFiscale !== '') {
-      console.log('codiceFiscale:', this.codiceFiscale);
-    }
-    if (this.partivaIva !== '') {
-      console.log('partivaIva:', this.partivaIva);
-    }
-    if (this.codiceSdi !== '') {
-      console.log('codiceSdi:', this.codiceSdi);
-    }
-    if (this.indirizzo !== '') {
-      console.log('indirizzo:', this.indirizzo);
-    }
-    if (this.citta !== '') {
-      console.log('citta:', this.citta);
-    }
-    if (this.cap !== '') {
-      console.log('cap:', this.cap);
-    }
-    if (this.provincia !== '') {
-      console.log('provincia:', this.provincia);
-    }
-    if (this.pec !== '') {
-      console.log('pec:', this.pec);
-    }
-    if (this.telefono !== '') {
-      console.log('telefono:', this.telefono);
-    }
-    if (this.cellulare !== '') {
-      console.log('cellulare:', this.cellulare);
-    }
-    if (this.spedizioneRagioneSociale !== '' && this.checkboxValue) {
-      console.log('spedizioneRagioneSociale:', this.spedizioneRagioneSociale);
-    }
-    if (this.spedizioneCognomeNome !== '' && this.checkboxValue) {
-      console.log('spedizioneCognomeNome:', this.spedizioneCognomeNome);
-    }
-    if (this.spedizioneIndirizzo !== '' && this.checkboxValue) {
-      console.log('spedizioneIndirizzo:', this.spedizioneIndirizzo);
-    }
-    if (this.spedizioneCitta !== '' && this.checkboxValue) {
-      console.log('spedizioneCitta:', this.spedizioneCitta);
-    }
-    if (this.spedizioneCap !== '' && this.checkboxValue) {
-      console.log('spedizioneCap:', this.spedizioneCap);
-    }
-    if (this.spedizioneNazione !== '' && this.checkboxValue) {
-      console.log('spedizioneNazione:', this.spedizioneNazione);
-    }
-    if (this.spedizioneProvincia !== '' && this.checkboxValue) {
-      console.log('spedizioneProvincia:', this.spedizioneProvincia);
-    }
-    if (this.spedizioneCellulare !== '' && this.checkboxValue) {
-      console.log('spedizioneCellulare:', this.spedizioneCellulare);
+    if (this.registerForm.valid) {
+      console.log('FormResults:', this.registerForm);
+    } else {
+      console.log('FormResults:',this.registerFormAzienda)
     }
   }
 
@@ -145,17 +128,10 @@ export class RegisterformComponent {
       this.totalSections--
       console.log("Checkbox disattivato");
       console.log(this.totalSections);
-      if (this.currentSection = 3) {
+      if (this.currentSection === 3) {
         this.currentSection = 2;
       }
     }
-  }
-
-  isInvalidCodiceFiscale: boolean = false;
-
-  verifyCodiceFiscale() {
-    const regex = /^[A-Z]{6}\d{2}[A-Z]\d{2}[A-Z]\d{3}[A-Z]$/;
-    this.isInvalidCodiceFiscale = !regex.test(this.codiceFiscale);
   }
 
 }
